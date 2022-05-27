d479effe6a06427c4e15d0991ec817b8a3483cf3
Ignacio Kruchowski <98991734+N-A-C-H-O@users.noreply.github.com>
2022-05-26 21:11:59 -0300

    Fix buttons

--- js/main.js
+++ js/main.js
@@ -21,22 +21,3 @@ const clearDisplay = () => {
 const deleteDisplay = () => {
     display.value = display.value.slice(0,-1)
 }
-
-btnClear.onclick = (clearDisplay());
-btnDel.onclick = deleteDisplay();
-btnPercent.onclick = calculate('%');
-btnDiv.onclick = calculate('/');
-btnSeven.onclick = (calculate(7));
-btnEight.onclick = calculate(8);
-btnNine.onclick = calculate(9);
-btnMul.onclick = calculate('*');
-btnFour.onclick = calculate(4);
-btnFive.onclick = calculate(5);
-btnSix.onclick = calculate(6);
-btnSub.onclick = calculate('-');
-btnOne.onclick = calculate(1);
-btnTwo.onclick = calculate(2);
-btnThree.onclick = calculate(3);
-btnAdd.onclick = calculate('+');
-btnDot.onclick = calculate('.');
-btnZero.onclick = calculate(0);
